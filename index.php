7c25cd6012b3ae21fa98863ea9ef3188bd1e48e5
ithirzty <45016834+ithirzty@users.noreply.github.com>
2019-05-05 19:00:42 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ include('fns.php');
 <html lang=fr>
 <head>
 <meta charset=utf-8>
+ <script src="//code.responsivevoice.org/responsivevoice.js?key=<?php echo $responsivevoiceapi; ?>"></script>
 <meta name=viewport content="width=device-width, initial-scale=1">
 <link href=https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css rel=stylesheet integrity=sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T crossorigin=anonymous>
 <link rel=stylesheet href=./weather-icons.css>
